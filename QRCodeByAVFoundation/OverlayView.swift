20a49182e2e9274febcddb6847826fee86185e7b
HMT@WDQ <(tommyyaphetshu@163.com)>
2015-06-26 08:59:50 +0800

    qrcode

---
+++ QRCodeByAVFoundation/OverlayView.swift
@@ -0,0 +1,21 @@
+//
+//  OverlayView.swift
+//  QRCodeByAVFoundation
+//
+//  Created by HMT on 15/5/21.
+//  Copyright (c) 2015年 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class OverlayView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
